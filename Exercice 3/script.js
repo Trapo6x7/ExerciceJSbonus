8fa4c8064bac5d7046f5e22c20dc47cc0322a687
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-19 16:24:19 +0100

    EXO3

---
+++ Exercice 3/script.js
@@ -0,0 +1,10 @@
+const text = document.querySelector("#text");
+const key1 = "r";
+
+document.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(event) {
+  if (event.key === key1) {
+    document.write("<p> BRAVOGUI </p>");
+  }
+}
---
+++ Exercice 4/script.js
@@ -0,0 +1,9 @@
+const boxes = document.querySelectorAll("#container");
+
+boxes.forEach((box) => {
+  box.addEventListener("mouseenter", handleRedBorder);
+});
+
+function handleRedBorder(){
+    
+}
