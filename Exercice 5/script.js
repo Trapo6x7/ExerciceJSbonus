584f0064fee920de11e0d4cb433186cc7354a273
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-20 14:00:03 +0100

    EXO4

---
+++ Exercice 5/script.js
@@ -0,0 +1,32 @@
+const rightButton = document.querySelector("#right-button");
+const leftButton = document.querySelector("#left-button");
+const img = document.querySelector("#biche")
+const biches = [
+  "./images/biche1.jpg",
+  "./images/biche2.jpg",
+  "./images/biche3.jpg",
+];
+let count = 0;
+
+rightButton.addEventListener("click", handleRightImageMove);
+leftButton.addEventListener("click", handleLeftImageMove);
+
+function handleRightImageMove() {
+  console.log(count);
+  if (count === biches.length - 1) {
+    count = 0;
+  } else {
+    count += 1;
+  }
+ img.src = biches[count]
+}
+
+function handleLeftImageMove() {
+  console.log(count);
+  if (count === 0) {
+    count = biches.length - 1;
+  } else {
+    count -= 1;
+  }
+  img.src = biches[count]
+}
